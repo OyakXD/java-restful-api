58769c06c3caa26eed44ba9c3c2dfb0e2f8c2da1
OyakXD <kayovieira157@gmail.com>
2024-05-21 16:24:47 -0300

    feat: Criando o projeto

---
+++ src/main/java/me/kayo/ProjectApiRestApplication.java
@@ -0,0 +1,13 @@
+package me.kayo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApiRestApplication.class, args);
+	}
+
+}
